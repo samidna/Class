b965f7e482f03e08a36b15b9f518ae0589effc16
SamidAghayev <samidna@code.edu.az>
2023-04-24 17:37:28 +0400

    created class task

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp4;
+Student stu1 = new("Samid","Aghayev","P137",85);
+Console.WriteLine(stu1.FullName());
+Console.WriteLine(stu1.Info());
+
+if (stu1.IsGraduated() ==true)
+{
+    Console.WriteLine("Ikinci imtahana haqq qazandiniz.");
+}
+  else
+{
+    Console.WriteLine("Ikinci imtahana haqq qazanmadiniz.");
+}
+Console.WriteLine(stu1.IsGraduated());
---
+++ Student.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp4;
+
+internal class Student
+{
+    public string Name;
+    public string Surname;
+    public double Point;
+    public string Group;
+    public Student(string name, string surname, string group, double point)
+    {
+        Name = name;
+        Surname = surname;
+        Point = point;
+        Group = group;
+    }
+    public string FullName()
+    {
+        return $"Name:{Name} Surname:{Surname}";
+    }
+    public string Info()
+    {
+        return $"{FullName()} Group:{Group} Point:{Point}";
+    }
+    public bool IsGraduated()
+    {
+        if(Point>=80)
+        {
+            return true ;
+        }
+            return false;
+    }        
+}
+
+
+
+
+
+
+
